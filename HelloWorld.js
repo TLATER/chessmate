62b3a6b4454c341678fe39822d81600d9fc66253
tlater <tm@tlater.net>
2015-01-29 21:43:52 +0000

    Yep, I'm starting to get this

--- HelloWorld.js
+++ HelloWorld.js
@@ -5,9 +5,13 @@ app.get('/', function (request, response) {
     response.send('Hello World');
 });
 
+app.get('/test', function (request, response) {
+    response.send('My my, this is cool :)')
+})
+
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-    
+
     console.log('Example app listening at http://%s%s', host, port);
 });
