d83560ffb1c565f2841d1949aebaf92db5c8d074
tlater <tm@tlater.net>
2015-01-29 21:48:43 +0000

    Some commenting and deleted unused test file

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,16 +1,28 @@
+//Require adds the "class", in this case express, our primitive but fast web
+// server
 var express = require('express');
+//App is an implementation of express, an object, if you want
 var app = express();
 
+//app.METHOD(PATH, HANDLER) - you should be able to decipher that
+//HANDLER is a function that is given a request and a response, both usable
+//objects, much like the java scanner
 app.get('/', function (request, response) {
     response.send('Hello World');
 });
 
+//Same as before, different folder
 app.get('/test', function (request, response) {
     response.send('My my, this is cool :)')
 })
 
+//This initializes the server to port 3000
 var server = app.listen(3000, function () {
+    
+    //This doesn't work for some reason
     var host = server.address().address;
+    
+    //This does
     var port = server.address().port;
 
     console.log('Example app listening at http://%s%s', host, port);
