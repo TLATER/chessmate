19be76570166b5755f2008d2143479e29e51fdb8
tlater <tm@tlater.net>
2015-01-30 14:04:59 +0000

    Hello World and routing for demonstration test chat

--- srv/routes/index.js
+++ srv/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+// GET test chat implementation
+router.get('/testChat', function(request, response) {
+    response.render('chat.ejs', { title: 'Hello World' });
 });
 
 module.exports = router;
