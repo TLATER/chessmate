4487741b5528a33cdb97e07adb8bbeb7b613a93f
tlater <tm@tlater.net>
2015-01-30 22:14:25 +0000

    It didn't. Back to debugging.

--- srv/public/javascripts/getMessages.js
+++ srv/public/javascripts/getMessages.js
@@ -2,6 +2,8 @@ var stream = new EventSource('/testChat/messageDistribute');
 stream.onmessage = function(response) {
     window.clientId = response.lastEventId;
 
+    console.log(response);
+
     var newText = document.createElement('div');
     newText.innerHTML = response.data;
     document.getElementById('display').appendChild(newText);
