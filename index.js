c42d74f31a906df4ff1a463ace097d1945c5016a
tlater <tm@tlater.net>
2015-01-30 19:05:18 +0000

    Test commit for client delete

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ var removeClient = function (id) {
 
 var receive = function (request, response) {
     console.log(request.body.id);
-    console.log(currentUsers[request.body.id].id);
+    console.log(currentUsers);
     currentUsers[request.body.id].resetLifetime();
 };
 
