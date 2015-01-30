ccd422d2d9e130afd1a7dfb7600bde7c7c9f8fed
tlater <tm@tlater.net>
2015-01-30 16:03:08 +0000

    Test commit for client delete

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var currentUsers = [];
 
 var registerClient = function (request, response) {
     var id = currentUsers.length + 1;
-    currentUsers.push(new Client(id, request, response));
-    currentUsers.push('something');
+    var newClient = new Client(id, request, response);
+    currentUsers.push(newClient);
 
     console.log(currentUsers[id]);
 
