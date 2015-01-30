61526b1a33a212ce3a2ddc526ec688074801f3c8
tlater <tm@tlater.net>
2015-01-30 15:56:19 +0000

    Test commit for client delete

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ var Client = require('./classes/client');
 
 var currentUsers = [];
 
-//Tests client every to see if they are still alive
-var removeClient = function (id) {
-    if (currentUsers[id].alive === 0)
-        currentUsers.splice(id, 1);
-
-    currentUsers[id].alive -= 1;
-};
-
 var registerClient = function (request, response) {
     var id = currentUsers.length + 1;
     currentUsers.push(new Client(id, request, response));
@@ -17,6 +9,14 @@ var registerClient = function (request, response) {
     currentUsers[id].timer = setInterval(removeClient(id), 10000);
 
     return id;
+};
+
+//Tests client every to see if they are still alive
+var removeClient = function (id) {
+    if (currentUsers[id].alive === 0)
+        currentUsers.splice(id, 1);
+
+    currentUsers[id].alive -= 1;
 };
 
 var receive = function (request, response) {
