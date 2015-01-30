97b3fe288f26d4c550aabc3a23eb9dd395cc1c9e
tlater <tm@tlater.net>
2015-01-30 15:23:45 +0000

    Test commit for client registration

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var registerClient = function (request, response) {
     var id = currentUsers.length + 1;
     currentUsers.push = new Client(id, request, response);
     console.log(currentUsers.length);
+    console.log(currentUsers[0]);
 
     return id;
 };
