d6f68d47b2f60285b18306b330a8cad93ad08ccd
tlater <tm@tlater.net>
2015-01-30 22:54:25 +0000

    Sorting out newly introduced bugs...

--- index.js
+++ index.js
@@ -27,17 +27,36 @@ var findById = function (id) {
 };
 
 var receive = function (request, response) {
-    var callingClient = findById(request.body.id);
+    var callingClient = findById(escapeHtml(request.body.id));
 
     callingClient.stayAlive();
 
-    if (!(request.body.msg === undefined)) {
-        var send = new Text(callingClient, request.body.msg, currentUsers);
+    if (!(escapeHtml(request.body.msg) === undefined)) {
+        console.log(escapeHtml(request.body.msg);
+        var send = new Text(callingClient, escapeHtml(request.body.msg), currentUsers);
         send.broadcast();
     }
     else
         response.send('Done');
 };
+
+//Credit where credit is due, this is useful, mate. github.com/kiprobinson,
+// http://stackoverflow.com/questions/1787322/
+//                     htmlspecialchars-equivalent-in-javascript/4835406#4835406
+function escapeHtml(text) {
+  if (text === undefined)
+    return undefined;
+
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
 
 exports.registerClient = registerClient;
 exports.receive = receive;
