23c72059fa5a13c2d841c102a80e805b20ea2a34
tlater <tm@tlater.net>
2015-01-31 13:23:25 +0000

    More changes

--- classes/messageDistribute.js
+++ classes/messageDistribute.js
@@ -11,11 +11,11 @@ var broadcast = function() {
     var string = this.client.id + "says: " + this.message;
 
     for (var i=0; i<this.room.length; i++) {
-        this.room[i].response.writeHead(200, {
-            'Content-Type': 'text/event-stream',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive'
-        });
+        // this.room[i].response.writeHead(200, {
+        //     'Content-Type': 'text/event-stream',
+        //     'Cache-Control': 'no-cache',
+        //     'Connection': 'keep-alive'
+        // });
 
         this.room[i].response.write('id:' + this.room[i].id + '\n\n');
         this.room[i].response.write('data:' + string + '\n\n');
