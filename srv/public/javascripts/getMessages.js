7c6f52a51383b7d9b82fa639d8c193683801dca1
tlater <tm@tlater.net>
2015-01-31 00:46:18 +0000

    Line seperation logic implemented

--- srv/public/javascripts/getMessages.js
+++ srv/public/javascripts/getMessages.js
@@ -5,6 +5,22 @@ stream.onmessage = function(response) {
     console.log(response);
 
     var newText = document.createElement('div');
+    if (response.data.length > 70) {
+        var lines = [response.data.slice(0, 70), response.data.slice(70)];
+
+        var i = 1;
+        var lastLine = lines[i];
+
+        while (lastLine > 70) {
+            lines[i+1] = lastLine.slice(70);
+            lastLine = lastLine.slice(0, 70);
+
+            i++;
+            lastLine = lines[i];
+        }
+
+        response.data = lines.join('\n');
+    }
     newText.innerHTML = response.data;
     document.getElementById('display').appendChild(newText);
 };
