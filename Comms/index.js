1e0e9fb3d0df23434e9d0a8be2826b6efe9ebda0
tlater <tm@tlater.net>
2015-02-10 23:25:08 +0000

    adsadsadas

--- Comms/index.js
+++ Comms/index.js
@@ -1,8 +1,9 @@
-var uiInteraction = require('uiInteraction');
+// The 'glue' file. Holds all communications together
+var uiCommunication() = require('uiCommunication');
 
 function server() {
     //{ something:something }
-    var object = new uiInteraction(request, response);
+    var object = new uiCommunication(request, response);
 }
 
 server.prototype.receive = function(request, response) {
