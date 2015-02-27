7bea9ca6adc26ec5f727f2e34d7f5967355602a6
tlater <tm@tlater.net>
2015-02-27 14:28:33 +0000

    adfg

---
+++ srv/socketMessage/chat.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+    
+        socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+    try
+    {
+ 
+        sendButton.onclick = function() {
+            if(name.value == "") {
+                alert("Please type your name!");
+            } else {
+                var text = field.value;
+                socket.emit('send', { message: text, username: name.value });
+            }
+        };
+    }
+    catch (error) {console.log(error)}
+ 
+}
