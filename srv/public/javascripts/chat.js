c551f6b685437dee64a948011bdf1b37cfcf87a9
tlater <tm@tlater.net>
2015-02-27 15:19:36 +0000

    Socket io testing

--- srv/public/javascripts/chat.js
+++ srv/public/javascripts/chat.js
@@ -1,31 +1,7 @@
-window.onload = function() {var messages = [];
-
-    var m = document.getElementById('m');
-    var sendButton = document.getElementById('sendButton');
+window.onload = function() {
     var socket = io.connect('http://www.tlater.net:3597');
-    var content = document.getElementById('messages');
     
-        socket.on('message', function (data) {
+    socket.on('message', function (data) {
         alert(data);
-        if(data.message) {
-            messages.push(data);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-                html += messages[i].message + '<br />';
-            }
-            content.innerHTML = html;
-        } else {
-            console.log("There is a problem:", data);
-        }
     });
-    try
-    {
- 
-        sendButton.onclick = function() {
-            var text = m.value;
-            socket.emit('send', { message: text });
-        };
-    }
-    catch (error) {console.log(error)}
 };
